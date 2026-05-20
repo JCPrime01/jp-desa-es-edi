e6a759d7eb02ee0c67d057d46c0b621c375209bb
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 21:48:04 -0300

    chore: update shareCode to 23RDWRZ9BZD

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=MDISMAA0FUX&afp=desafio' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=23RDWRZ9BZD&afp=desafio' });
     res.end();
   }
